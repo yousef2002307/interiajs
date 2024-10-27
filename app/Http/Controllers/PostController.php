5a8b236df4885e9c114b94314d5d67709b996f47
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2024-10-27 12:39:17 +0300

    new commit3 thursday 24oct

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,6 +39,7 @@ class PostController extends Controller
         Post::create([
             "body" => $validatedData['body']
         ]);
+        //////////debugging
         return redirect('/');
  
     }
