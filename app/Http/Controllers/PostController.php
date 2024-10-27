be4ab55076fd079cecd3ff250904cd6dafcb3fce
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2024-10-27 12:40:08 +0300

    new commit3 thursday 24oct

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,11 +49,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-<<<<<<< HEAD
         return inertia("Show");
-=======
-        return inertia("Home/Home");
->>>>>>> a07c6bf2c276936df39c0001a34476c671eb52e8
     }
 
     /**
